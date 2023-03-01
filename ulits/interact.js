993f8add06bdb3eef8cf408f2f644efac7d5b3d2
rarestudios-stack <rarestudisnewyork@protonmail.com>
2023-03-01 00:41:47 -0800

    fixing

--- ulits/interact.js
+++ ulits/interact.js
@@ -1,7 +1,7 @@
-const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
+// const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
 import { config } from '../dapp.config'
 
-const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL)
+// const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL)
 const contract = require('../artifacts/contracts/Skelee.sol/Skelee.json')
 const nftContract = new web3.eth.Contract(contract.abi, config.contractAddress)
 
