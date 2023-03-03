d802f6c7cc8b73e43becbdf596d614fbda7861bf
rarestudios-stack <rarestudisnewyork@protonmail.com>
2023-03-03 03:04:46 -0800

    css fix

--- components/navtabscopy.js
+++ components/navtabscopy.js
@@ -228,7 +228,7 @@ className='w-auto h-[30px] cursor-pointer'
 
 <div className={!nav?'absolute w-full md:top-[90px] top-[110px] flex flex-col items-center ':'hidden'}>
 <div className='h-screen flex flex-col items-center'>
-  <img src='/skelee-homepage-icon.png' className='md:h-[300px] h-[150px] w-auto md:mt-16 mt-[220px]'/>
+  <img src='/skelee-homepage-icon.png' className='md:h-[300px] h-[150px] w-auto md:mt-16 mt-[200px]'/>
   <h1 className='text-[18px] tracking-wide text-center max-w-screen mt-8 font-LuckiestGuy uppercase text-white'>
     Welcome<br/>
     to the world of Skelee. <br/>
