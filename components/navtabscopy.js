c6af3d6175f320b9c0e0f19187c75f3aab10dcf4
rarestudios-stack <rarestudisnewyork@protonmail.com>
2023-03-03 02:39:36 -0800

    css fix

--- components/navtabscopy.js
+++ components/navtabscopy.js
@@ -126,8 +126,8 @@ export default function Navtabs (){
      </div>
      </a>
      <a href='/'>
-     <div className='px-5 py-1 text-[13px] bg-black hover:bg-white text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>
-     <svg xmlns="http://www.w3.org/2000/svg" className='hover:fill-black fill-white' width="20" height="20" viewBox="0 0 24 24"><path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"></path></svg>
+     <div className='px-5 py-1 text-[13px] hover:fill-black fill-white bg-black hover:bg-white text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>
+     <svg xmlns="http://www.w3.org/2000/svg" className='' fill='currentColor' width="20" height="20" viewBox="0 0 24 24"><path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"></path></svg>
      </div>
      </a>
      {/*
@@ -228,7 +228,7 @@ className='w-auto h-[30px] cursor-pointer'
 
 <div className={!nav?'absolute w-full md:top-[90px] top-[110px] flex flex-col items-center ':'hidden'}>
 <div className='h-screen flex flex-col items-center'>
-  <img src='/skelee-homepage-icon.png' className='md:h-[40px] h-[25px] w-auto mt-8'/>
+  <img src='/skelee-homepage-icon.png' className='md:h-[100px] h-[50px] w-auto mt-8'/>
   <h1 className='text-[18px] tracking-wide text-center max-w-screen mt-8 font-LuckiestGuy uppercase text-white'>
     Welcome<br/>
     to the world of Skelee <br/>
