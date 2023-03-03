f76d05d040c15a3ef999aade4d260396abc35232
rarestudios-stack <rarestudisnewyork@protonmail.com>
2023-03-03 04:24:43 -0800

    css fix

--- components/FAQs.js
+++ components/FAQs.js
@@ -14,8 +14,8 @@ export default function FAQs() {
   };
  
   const customAnimation = {
-    mount: { scale: 1 },
-    unmount: { scale: 0.9 },
+    mount: { scale: 1 , opacity : 1 },
+    unmount: { scale: 0.9, opacity : 0 },
   };
  
   return (
